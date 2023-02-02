e514a20f4ecdeabdc32e043771b05ff11414983b
Michael Harris <mharrisjr01@gmail.com>
2023-02-02 17:10:10 +0000

    Completed Lab Instantiation

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object object1 = new Object();
+        return object1;
     }
 }
